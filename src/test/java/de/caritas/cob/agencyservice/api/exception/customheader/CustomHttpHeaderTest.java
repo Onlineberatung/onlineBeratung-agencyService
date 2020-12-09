e03bf44ce102106df2244f1039e95f4544abf900
Daniel Hillwig <hillwig@open4business.de>
2020-12-09 08:35:21 +0100

    feat: added tests for CustomHttpHeader

---
+++ src/test/java/de/caritas/cob/agencyservice/api/exception/customheader/CustomHttpHeaderTest.java
@@ -0,0 +1,28 @@
+package de.caritas.cob.agencyservice.api.exception.customheader;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import de.caritas.cob.agencyservice.api.exception.httpresponses.HttpStatusExceptionReason;
+import org.junit.Test;
+import org.springframework.http.HttpHeaders;
+
+public class CustomHttpHeaderTest {
+
+  @Test
+  public void buildHeader_Should_ReturnHttpHeadersWithXReason() {
+
+    CustomHttpHeader customHttpHeader = new CustomHttpHeader(HttpStatusExceptionReason.INVALID_CONSULTING_TYPE);
+    HttpHeaders result = customHttpHeader.buildHeader();
+    assertTrue(result.containsKey("X-Reason"));
+
+  }
+
+  @Test
+  public void buildHeader_Should_ReturnCorrectHttpStatusExceptionReason() {
+
+    CustomHttpHeader customHttpHeader = new CustomHttpHeader(HttpStatusExceptionReason.INVALID_CONSULTING_TYPE);
+    HttpHeaders result = customHttpHeader.buildHeader();
+    assertEquals(HttpStatusExceptionReason.INVALID_CONSULTING_TYPE.toString(), result.get("X-Reason").get(0));
+  }
+
+}
