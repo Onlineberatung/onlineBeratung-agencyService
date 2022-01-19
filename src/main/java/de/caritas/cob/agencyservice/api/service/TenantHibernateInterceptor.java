445464e5a1446876b4b5fdf8510e02a9d73639a0
aalicic <adnan.alicic@dccs.at>
2022-01-19 17:31:47 +0100

    VIC-35: Make all data accessible per tenant Added integration test for tenants

--- src/main/java/de/caritas/cob/agencyservice/api/service/TenantHibernateInterceptor.java
+++ src/main/java/de/caritas/cob/agencyservice/api/service/TenantHibernateInterceptor.java
@@ -3,11 +3,10 @@ package de.caritas.cob.agencyservice.api.service;
 import de.caritas.cob.agencyservice.api.repository.agency.Agency;
 import de.caritas.cob.agencyservice.api.repository.agencypostcoderange.AgencyPostcodeRange;
 import de.caritas.cob.agencyservice.filter.TenantContext;
-import java.io.Serializable;
+import java.util.Iterator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.EmptyInterceptor;
-import org.hibernate.type.Type;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,16 +15,18 @@ import org.springframework.stereotype.Service;
 public class TenantHibernateInterceptor extends EmptyInterceptor {
 
   @Override
-  public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames,
-      Type[] types) {
-    if (entity instanceof Agency) {
-      ((Agency) entity).setTenantId(TenantContext.getCurrentTenant());
-    }
-    if (entity instanceof AgencyPostcodeRange) {
-      ((AgencyPostcodeRange) entity).setTenantId(TenantContext.getCurrentTenant());
+  public void preFlush(Iterator entities) {
+    Object entity;
+    while (entities.hasNext()) {
+      entity = entities.next();
+      if (entity instanceof Agency) {
+        ((Agency) entity).setTenantId(TenantContext.getCurrentTenant());
+      }
+      if (entity instanceof AgencyPostcodeRange) {
+        ((AgencyPostcodeRange) entity).setTenantId(TenantContext.getCurrentTenant());
+      }
     }
 
-    return super.onSave(entity, id, state, propertyNames, types);
+    super.preFlush(entities);
   }
-
 }
