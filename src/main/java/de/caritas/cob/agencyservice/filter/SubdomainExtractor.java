e613ce9e6adeacc4ada4f8bcd3be55fdccebed3b
tkuzynow <tomasz.kuzynowski@gmail.com>
2022-01-20 08:28:27 +0100

    fix test properties

--- src/main/java/de/caritas/cob/agencyservice/filter/SubdomainExtractor.java
+++ src/main/java/de/caritas/cob/agencyservice/filter/SubdomainExtractor.java
@@ -2,7 +2,6 @@ package de.caritas.cob.agencyservice.filter;
 
 import com.google.common.net.InternetDomainName;
 import java.util.Optional;
-import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -11,16 +10,16 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 public class SubdomainExtractor {
 
   public Optional<String> getCurrentSubdomain() {
-    HttpServletRequest request =
+    var request =
         ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
             .getRequest();
     return extractSubdomain(request.getServerName());
   }
 
   Optional<String> extractSubdomain(String site) {
-    InternetDomainName domainName = InternetDomainName.from(site);
+    var domainName = InternetDomainName.from(site);
     if (domainName.hasParent()) {
-      String subDomain = site.replaceAll(domainName.topPrivateDomain().toString(), "");
+      var subDomain = site.replaceAll(domainName.topPrivateDomain().toString(), "");
       return Optional.of(subDomain.substring(0, subDomain.length() - 1));
     } else {
       return Optional.empty();
