44eb371e1cd6668c225dc9b4c911c7b6bb7775d2
Markus Eberl <eberl@open4business.de>
2022-02-18 07:57:03 +0100

    chore: Use junit 5 for all failing tests

--- src/test/java/de/caritas/cob/agencyservice/api/admin/hallink/AgencyLinksBuilderTest.java
+++ src/test/java/de/caritas/cob/agencyservice/api/admin/hallink/AgencyLinksBuilderTest.java
@@ -3,22 +3,25 @@ package de.caritas.cob.agencyservice.api.admin.hallink;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import de.caritas.cob.agencyservice.api.model.AgencyLinks;
 import de.caritas.cob.agencyservice.api.model.HalLink.MethodEnum;
 import de.caritas.cob.agencyservice.api.repository.agency.Agency;
 import org.jeasy.random.EasyRandom;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-public class AgencyLinksBuilderTest {
+@ExtendWith(MockitoExtension.class)
+class AgencyLinksBuilderTest {
 
   @Test
-  public void buildAgencyLinks_Should_returnAgencyLinks_When_AgencyIsSet() {
+  void buildAgencyLinks_Should_returnAgencyLinks_When_AgencyIsSet() {
 
-    EasyRandom easyRandom = new EasyRandom();
-    Agency agency = easyRandom.nextObject(Agency.class);
+    var easyRandom = new EasyRandom();
+    var agency = easyRandom.nextObject(Agency.class);
 
-    AgencyLinks agencyLinks = AgencyLinksBuilder.getInstance(agency)
+    var agencyLinks = AgencyLinksBuilder.getInstance(agency)
         .buildAgencyLinks();
 
     assertThat(agencyLinks, notNullValue());
@@ -41,11 +44,11 @@ public class AgencyLinksBuilderTest {
 
   }
 
-  @Test(expected = NullPointerException.class)
-  public void buildAgencyLinks_Should_ThrowNullPointerException_WhenAgencyIsNotSet() {
+  @Test
+  void buildAgencyLinks_Should_ThrowNullPointerException_WhenAgencyIsNotSet() {
+    var builder = AgencyLinksBuilder.getInstance(null);
 
-    AgencyLinksBuilder.getInstance(null).buildAgencyLinks();
-
+    assertThrows(NullPointerException.class, builder::buildAgencyLinks);
   }
 
 }
