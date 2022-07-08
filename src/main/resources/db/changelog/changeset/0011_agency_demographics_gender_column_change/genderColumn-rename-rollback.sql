805b3652bdcc8e7a2c25d06a7953706179852134
tkuzynow <tomasz.kuzynowski@gmail.com>
2022-07-08 11:15:53 +0200

    feat: rename to plural form for gender column

--- src/main/resources/db/changelog/changeset/0011_agency_demographics_gender_column_change/genderColumn-rename-rollback.sql
+++ src/main/resources/db/changelog/changeset/0011_agency_demographics_gender_column_change/genderColumn-rename-rollback.sql
@@ -1,3 +1,2 @@
-ALTER TABLE `agencyservice`.`agency`
 ALTER TABLE `agencyservice`.`agency` RENAME COLUMN genders TO gender;
 
--- src/main/resources/db/changelog/changeset/0011_agency_demographics_gender_column_change/genderColumn-rename.sql
+++ src/main/resources/db/changelog/changeset/0011_agency_demographics_gender_column_change/genderColumn-rename.sql
@@ -1,2 +1,1 @@
-ALTER TABLE `agencyservice`.`agency`
 ALTER TABLE `agencyservice`.`agency` RENAME COLUMN gender TO genders;
