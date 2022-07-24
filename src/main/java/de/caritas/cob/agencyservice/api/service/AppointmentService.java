6c16ac6fdf9273ff5099b5d63118b2997ab802c5
aalicic <adnan@leads.ba>
2022-07-24 18:12:44 +0200

    feat/VIC-837-Admin-can-create-and-edit-team

--- src/main/java/de/caritas/cob/agencyservice/api/service/AppointmentService.java
+++ src/main/java/de/caritas/cob/agencyservice/api/service/AppointmentService.java
@@ -1,11 +1,14 @@
 package de.caritas.cob.agencyservice.api.service;
 
 import de.caritas.cob.agencyservice.api.repository.agency.Agency;
+import de.caritas.cob.agencyservice.api.service.securityheader.SecurityHeaderSupplier;
+import de.caritas.cob.agencyservice.appointmentservice.generated.ApiClient;
 import de.caritas.cob.agencyservice.appointmentservice.generated.web.AgencyApi;
 import de.caritas.cob.agencyservice.appointmentservice.generated.web.model.AgencyMasterDataSyncRequestDTO;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +16,8 @@ import org.springframework.stereotype.Service;
 public class AppointmentService {
 
   private final @NonNull AgencyApi appointmentAgencyApi;
+  private final @NonNull SecurityHeaderSupplier securityHeaderSupplier;
+  private final @NonNull TenantHeaderSupplier tenantHeaderSupplier;
 
   @Value("${feature.appointment.enabled}")
   private boolean appointmentFeatureEnabled;
@@ -24,6 +29,7 @@ public class AppointmentService {
     AgencyMasterDataSyncRequestDTO request = new AgencyMasterDataSyncRequestDTO();
     request.setId(agency.getId());
     request.setName(agency.getName());
+    addDefaultHeaders(appointmentAgencyApi.getApiClient());
     appointmentAgencyApi.agencyMasterDataSync(request);
   }
 
@@ -31,8 +37,15 @@ public class AppointmentService {
     if (!appointmentFeatureEnabled) {
       return;
     }
+    addDefaultHeaders(appointmentAgencyApi.getApiClient());
     appointmentAgencyApi.deleteAgency(agency.getId());
   }
 
+
+  protected void addDefaultHeaders(ApiClient apiClient) {
+    HttpHeaders headers = this.securityHeaderSupplier.getKeycloakAndCsrfHttpHeaders();
+    tenantHeaderSupplier.addTenantHeader(headers);
+    headers.forEach((key, value) -> apiClient.addDefaultHeader(key, value.iterator().next()));
+  }
 
 }
