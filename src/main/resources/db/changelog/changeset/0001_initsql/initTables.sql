1af0663e4cbf17ef9b7de744b70f56abb4bb157a
tkuzynow <tomasz.kuzynowski@gmail.com>
2022-10-28 11:43:35 +0200

    fix: fix mysql incopmpatibilities

--- src/main/resources/db/changelog/changeset/0001_initsql/initTables.sql
+++ src/main/resources/db/changelog/changeset/0001_initsql/initTables.sql
@@ -2,8 +2,8 @@ CREATE TABLE `agencyservice`.`diocese` (
   `id` bigint(21) NOT NULL,
   `name` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `id_old` bigint(21) NOT NULL,
-  `create_date` datetime NOT NULL DEFAULT UTC_TIMESTAMP(),
-  `update_date` datetime NOT NULL DEFAULT UTC_TIMESTAMP(),
+  `create_date` datetime NOT NULL DEFAULT UTC_TIMESTAMP,
+  `update_date` datetime NOT NULL DEFAULT UTC_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 CREATE SEQUENCE agencyservice.sequence_diocese
