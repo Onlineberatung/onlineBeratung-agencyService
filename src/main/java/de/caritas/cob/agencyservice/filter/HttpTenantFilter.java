2e93af614c7c1dc330655805b1cd3e2509f96146
tkuzynow <tomasz.kuzynowski@gmail.com>
2023-04-18 12:23:22 +0200

    feat: add healtcheck

--- src/main/java/de/caritas/cob/agencyservice/filter/HttpTenantFilter.java
+++ src/main/java/de/caritas/cob/agencyservice/filter/HttpTenantFilter.java
@@ -3,6 +3,9 @@ package de.caritas.cob.agencyservice.filter;
 import de.caritas.cob.agencyservice.api.tenant.TenantContext;
 import de.caritas.cob.agencyservice.api.tenant.TenantResolverService;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -10,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -25,14 +29,41 @@ public class HttpTenantFilter extends OncePerRequestFilter {
 
   private final TenantResolverService tenantResolverService;
 
+  private static final String[] TENANCY_FILTER_WHITELIST =
+      new String[] {"/actuator/health", "/actuator/health/**", "/swagger-ui.html", "/favicon.ico"};
+
+  private final DefaultRequiresTenantFilterMatcher requiresTenantFilterMatcher =
+      new DefaultRequiresTenantFilterMatcher();
+
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
       FilterChain filterChain) throws ServletException, IOException {
-    log.debug("Trying to resolve tenant for request coming from URI {}", request.getRequestURI());
-    Long tenantId = tenantResolverService.resolve(request);
-    log.debug("Setting current tenant context to: " + tenantId);
-    TenantContext.setCurrentTenant(tenantId);
-    filterChain.doFilter(request, response);
-    TenantContext.clear();
+    if (requiresTenantFilterMatcher.matches(request)) {
+      log.debug("Trying to resolve tenant for request coming from URI {}", request.getRequestURI());
+      Long tenantId = tenantResolverService.resolve(request);
+      log.debug("Setting current tenant context to: " + tenantId);
+      TenantContext.setCurrentTenant(tenantId);
+      filterChain.doFilter(request, response);
+      TenantContext.clear();
+    } else {
+      log.debug(
+          "Skipping tenant filter for request: {} as it belongs to a tenancy whitelist.",
+          request.getRequestURI());
+      filterChain.doFilter(request, response);
+    }
+  }
+
+  class DefaultRequiresTenantFilterMatcher implements RequestMatcher {
+    @Override
+    public boolean matches(HttpServletRequest request) {
+
+      List<String> tenantWhitelist = new ArrayList<>(Arrays.asList(TENANCY_FILTER_WHITELIST));
+      return !belongsToWhitelist(request, tenantWhitelist);
+    }
+
+    private boolean belongsToWhitelist(HttpServletRequest request, List<String> tenantWhitelist) {
+      return tenantWhitelist.parallelStream()
+          .anyMatch(request.getRequestURI().toLowerCase()::contains);
+    }
   }
 }
