6a762717c752331fff90275f2889d9a28163be14
tkuzynow <tomasz.kuzynowski@gmail.com>
2023-11-09 13:42:01 +0100

    fix: fix migration script

--- src/main/resources/db/changelog/changeset/0016_add_data_protection_attributes/addDataProtectionAttributes-rollback.sql
+++ src/main/resources/db/changelog/changeset/0016_add_data_protection_attributes/addDataProtectionAttributes-rollback.sql
@@ -1,2 +1,8 @@
 ALTER TABLE `agencyservice`.`agency`
 DROP COLUMN data_protection_officer_contact longtext;
+
+ALTER TABLE `agencyservice`.`agency`
+DROP COLUMN data_protection_alternative_contact longtext;
+
+ALTER TABLE `agencyservice`.`agency`
+DROP COLUMN data_protection_agency_contact longtext;
--- src/main/resources/db/changelog/changeset/0016_add_data_protection_attributes/addDataProtectionAttributes.sql
+++ src/main/resources/db/changelog/changeset/0016_add_data_protection_attributes/addDataProtectionAttributes.sql
@@ -2,7 +2,7 @@ ALTER TABLE `agencyservice`.`agency`
 ADD COLUMN data_protection_responsible_entity varchar(100) NULL AFTER `counselling_relations`;
 
 ALTER TABLE `agencyservice`.`agency`
-ADD COLUMN data_protection_responsible_contact longtext NULL AFTER `data_protection_responsible_entity`;
+ADD COLUMN data_protection_alternative_contact longtext NULL AFTER `data_protection_responsible_entity`;
 
 ALTER TABLE `agencyservice`.`agency`
 ADD COLUMN data_protection_officer_contact longtext NULL AFTER `data_protection_responsible_contact`;
