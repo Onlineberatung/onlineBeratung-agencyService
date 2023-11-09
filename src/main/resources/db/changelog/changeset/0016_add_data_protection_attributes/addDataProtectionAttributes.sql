50fa7f8fbf9050a322eb26522ed9175a57cbad43
tkuzynow <tomasz.kuzynowski@gmail.com>
2023-11-09 13:56:17 +0100

    fix: fix SQL script

--- src/main/resources/db/changelog/changeset/0016_add_data_protection_attributes/addDataProtectionAttributes.sql
+++ src/main/resources/db/changelog/changeset/0016_add_data_protection_attributes/addDataProtectionAttributes.sql
@@ -5,7 +5,7 @@ ALTER TABLE `agencyservice`.`agency`
 ADD COLUMN data_protection_alternative_contact longtext NULL AFTER `data_protection_responsible_entity`;
 
 ALTER TABLE `agencyservice`.`agency`
-ADD COLUMN data_protection_officer_contact longtext NULL AFTER `data_protection_responsible_contact`;
+ADD COLUMN data_protection_officer_contact longtext NULL AFTER `data_protection_alternative_contact`;
 
 ALTER TABLE `agencyservice`.`agency`
 ADD COLUMN data_protection_agency_contact longtext NULL AFTER `data_protection_officer_contact`;
