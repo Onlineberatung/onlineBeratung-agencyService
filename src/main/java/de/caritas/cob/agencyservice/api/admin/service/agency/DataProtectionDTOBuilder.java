f58920a9578532576287ffaee22a7f0bf625d9c2
tkuzynow <tomasz.kuzynowski@gmail.com>
2023-12-22 13:15:32 +0100

    fix: converter

--- src/main/java/de/caritas/cob/agencyservice/api/admin/service/agency/DataProtectionDTOBuilder.java
+++ src/main/java/de/caritas/cob/agencyservice/api/admin/service/agency/DataProtectionDTOBuilder.java
@@ -4,7 +4,9 @@ import de.caritas.cob.agencyservice.api.model.DataProtectionContactDTO;
 import de.caritas.cob.agencyservice.api.model.DataProtectionDTO;
 import de.caritas.cob.agencyservice.api.model.DataProtectionDTO.DataProtectionResponsibleEntityEnum;
 import de.caritas.cob.agencyservice.api.repository.agency.Agency;
+import de.caritas.cob.agencyservice.api.repository.agency.DataProtectionResponsibleEntity;
 import de.caritas.cob.agencyservice.api.util.JsonConverter;
+import java.util.function.Supplier;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 
@@ -13,60 +15,45 @@ public class DataProtectionDTOBuilder {
 
   private final @NonNull Agency agency;
 
-
   public DataProtectionDTO fromAgency() {
     if (agency.getDataProtectionResponsibleEntity() == null) {
       return null;
     }
 
-    switch (this.agency.getDataProtectionResponsibleEntity()) {
+    return new DataProtectionDTO()
+        .dataProtectionResponsibleEntity(convertEnums(agency.getDataProtectionResponsibleEntity()))
+        .agencyDataProtectionResponsibleContact(
+            convertNullSafe(agency::getDataProtectionAgencyResponsibleContactData))
+        .alternativeDataProtectionRepresentativeContact(
+            convertNullSafe(agency::getDataProtectionAlternativeContactData))
+        .dataProtectionOfficerContact(
+            convertNullSafe(agency::getDataProtectionOfficerContactData));
+  }
+
+  private DataProtectionContactDTO convertNullSafe(Supplier<String> dataProtectionDataSupplier) {
+    if (dataProtectionDataSupplier.get() == null) {
+      return null;
+    } else {
+      return JsonConverter.convertFromJson(dataProtectionDataSupplier.get());
+    }
+  }
+
+
+  private static DataProtectionResponsibleEntityEnum convertEnums(
+      DataProtectionResponsibleEntity dataProtectionEntity) {
+
+    switch (dataProtectionEntity) {
       case AGENCY_RESPONSIBLE:
-        return getAgencyResponsibleDataProtectionDTO();
-      case ALTERNATIVE_REPRESENTATIVE:
-        return getAlternativeRepresentative();
+        return DataProtectionDTO.DataProtectionResponsibleEntityEnum.AGENCY_RESPONSIBLE;
       case DATA_PROTECTION_OFFICER:
-        return getDataProtectionOfficerDTO();
+        return DataProtectionDTO.DataProtectionResponsibleEntityEnum.DATA_PROTECTION_OFFICER;
+      case ALTERNATIVE_REPRESENTATIVE:
+        return DataProtectionDTO.DataProtectionResponsibleEntityEnum.ALTERNATIVE_REPRESENTATIVE;
       default:
-        return null;
+        throw new IllegalArgumentException(
+            "DataProtectionResponsibleEntity not supported: " + dataProtectionEntity);
     }
   }
 
-  private DataProtectionDTO getDataProtectionOfficerDTO() {
-    String dataProtectionOfficerContactData = this.agency.getDataProtectionOfficerContactData();
-    if (dataProtectionOfficerContactData == null) {
-      return null;
-    }
-    DataProtectionContactDTO dataProtectionContactDTO = JsonConverter.convertFromJson(
-        dataProtectionOfficerContactData);
-    return new DataProtectionDTO()
-        .dataProtectionResponsibleEntity(
-            DataProtectionResponsibleEntityEnum.DATA_PROTECTION_OFFICER)
-        .dataProtectionOfficerContact(dataProtectionContactDTO);
-  }
 
-  private DataProtectionDTO getAlternativeRepresentative() {
-    String dataProtectionAlternativeContactData = this.agency.getDataProtectionAlternativeContactData();
-    if (dataProtectionAlternativeContactData == null) {
-      return null;
-    }
-    DataProtectionContactDTO dataProtectionContactDTO = JsonConverter.convertFromJson(
-        dataProtectionAlternativeContactData);
-
-    return new DataProtectionDTO()
-        .dataProtectionResponsibleEntity(
-            DataProtectionResponsibleEntityEnum.ALTERNATIVE_REPRESENTATIVE)
-        .alternativeDataProtectionRepresentativeContact(dataProtectionContactDTO);
-  }
-
-  private DataProtectionDTO getAgencyResponsibleDataProtectionDTO() {
-    String dataProtectionAgencyResponsibleContactData = this.agency.getDataProtectionAgencyResponsibleContactData();
-    if (dataProtectionAgencyResponsibleContactData == null) {
-      return null;
-    }
-    DataProtectionContactDTO dataProtectionContactDTO = JsonConverter.convertFromJson(
-        dataProtectionAgencyResponsibleContactData);
-    return new DataProtectionDTO()
-        .dataProtectionResponsibleEntity(DataProtectionResponsibleEntityEnum.AGENCY_RESPONSIBLE)
-        .agencyDataProtectionResponsibleContact(dataProtectionContactDTO);
-  }
 }
